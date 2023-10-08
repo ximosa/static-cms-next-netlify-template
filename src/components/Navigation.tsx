1fa2eb8cc10b25ddad6f67ddfc9fd74778e971e0
ximo <ximosa@gmail.com>
2023-10-08 16:51:28 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,7 +15,7 @@ const Navigation: FC = () => {
     <>
       <Burger active={active} onClick={() => setActive(!active)} />
       <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div className="w-0 md:w-28 md:block">
+      <div className="flex items-center flex-shrink-0 text-white mr-6"">
         <ul
           className={classNames(
             `
@@ -43,12 +43,12 @@ const Navigation: FC = () => {
             active && "opacity-100 translate-y-0"
           )}
         >
-          <li className="mb-7 text-3xl p-0 pr-6 last:mb-0 md:text-lg md:pr-0">
+          <li className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
             <Link className={classNames(pathname === "/" && "text-gray-900 font-bold")} href="/">
               Sobre mi...
             </Link>
           </li>
-          <li className="mb-7 text-3xl p-0 pr-6 last:mb-0 md:text-lg md:pr-0">
+          <li className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
             <Link className={classNames(pathname.startsWith("/posts") && "text-gray-900 font-bold")} href="/posts">
               Blog
             </Link>
