efbdc3d1ce5618488ea580f8fe38a5ab3858e598
ximo <ximosa@gmail.com>
2023-10-08 16:58:21 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,7 +15,7 @@ const Navigation: FC = () => {
     <>
       <Burger active={active} onClick={() => setActive(!active)} />
       <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6"">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
         <ul
           className={classNames(
             `
